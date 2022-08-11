32f9594970c2c10ff3b3427df074f7e120ace8a8
zzong36 <100885809+zzong36@users.noreply.github.com>
2022-08-11 15:05:36 +0900

    a

--- spring-mvc/src/main/java/kr/ac/kopo/resbody/ResBodyController.java
+++ spring-mvc/src/main/java/kr/ac/kopo/resbody/ResBodyController.java
@@ -1,14 +1,18 @@
 package kr.ac.kopo.resbody;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import kr.ac.kopo.member.MemberVO;
 
+//@RestController (밑에 responsebody는 지워야 한다)
 @Controller
 public class ResBodyController {
 	
@@ -37,6 +41,30 @@ public class ResBodyController {
 		member.setName("박길동");
 		member.setPassword("123456");
 		return member;
+	}
+	
+	@RequestMapping("/resStringListBody.json")
+	@ResponseBody
+	public List<String> resJsonStringListBody(){
+		List<String> list = new ArrayList<>();
+		for(int i = 1; i < 4; i++) {
+			list.add(String.valueOf(i));
+		}
+		return list;
+	}
+	
+	@RequestMapping("/resVOListBody.json")
+	@ResponseBody
+	public List<MemberVO> resJsonVOListBody(){
+		List<MemberVO> list = new ArrayList<>();
+		for(int i = 1; i < 4; i++) {
+			MemberVO member = new MemberVO();
+			member.setId("hong" + i);
+			member.setName("홍길동" + i);
+			member.setPassword("1234" + i);
+			list.add(member);
+		}
+		return list;
 	}
 
 }
--- spring-mvc/src/main/webapp/index.jsp
+++ spring-mvc/src/main/webapp/index.jsp
@@ -13,6 +13,8 @@
 <a href="<%= request.getContextPath()%>/resBody.do">responseBody</a><br>
 <a href="<%= request.getContextPath()%>/resBody.json">Json 응답1</a><br>
 <a href="<%= request.getContextPath()%>/resVOBody.json">Json 응답2</a><br>
+<a href="<%= request.getContextPath()%>/resStringListBody.json">Json List(문자열) 응답</a><br>
+<a href="<%= request.getContextPath()%>/resVOListBody.json">Json 회원리스트 응답</a><br>
 
 </body>
 </html>
